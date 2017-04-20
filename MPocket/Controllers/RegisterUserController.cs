50d64991aca090994083805dc6454c9009b23206
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-04-20 14:01:23 +0200

    register window

---
+++ MPocket/Controllers/RegisterUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MPocket.Controllers
+{
+    public class RegisterUserController : Controller
+    {
+        // GET: RegisterUser
+        public ActionResult RegisterUserView()
+        {
+            return View();
+        }
+    }
+}
